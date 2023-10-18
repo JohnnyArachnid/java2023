76ae327f1a70ebae6c1e893c60cd5ad552d7de52
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2023-10-18 19:37:07 +0200

    Zmiany

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,4 @@
 package org.example;
-
-import org.leapyear.LeapYearCalculator;
-
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
--- src/test/java/org/leapyear/Main.java
+++
@@ -1,9 +0,0 @@
-package org.leapyear;
-
-public class Main {
-  public static void main(String[] args) {
-    LeapYearCalculatorTest.testLeapYear();
-    LeapYearCalculatorTest.testOutOfRangeYear();
-    LeapYearCalculatorTest.testNonLeapYear();
-  }
-}
