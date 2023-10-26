eb90e388bba0514c922aa9ad4be67599014b55ef
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2023-10-26 12:27:33 +0200

    Zmiany

--- src/main/java/org/life/LifeSimulator.java
+++ src/main/java/org/life/LifeSimulator.java
@@ -3,19 +3,19 @@ package org.life;
 public class LifeSimulator {
 
   public static void main(String[] args) {
-    Board board = new Board(10, 10);
-    /* TODO:
-     - Add at least 2 classes that implement the Organism interface.
-     - The new classes should possess unique abilities, such as:
-        -- Jumping (moving more than 1 step at a time).
-        -- Sight (detecting other organisms within a certain radius).
-        -- Avoiding illegal moves.
-     - Ensure that an Organism doesn't move if it attempts an illegal move.
-     - Implement a mechanism where an Organism consumes another (taking all its energy) when it occupies the same space.
-     - Run simulation for some time, eg. when there is only one Organism left
-     */
-    Organism organism = new Organism(100);
-    board.addOrganism(organism, 5, 5);
-    organism.move();
+    Board board = new Board(5, 5);
+    Organism organism1 = new Organism(Organism.random.nextInt(100)+1, 1, "Basic");
+    Sportsman organism2 = new Sportsman(Organism.random.nextInt(100)+1, 2, "Sportsman");
+    Scientist organism3 = new Scientist(Organism.random.nextInt(100)+1, 3, "Scientist");
+    board.addOrganism(organism1, 0, 0);
+    board.addOrganism(organism2, 2, 2);
+    board.addOrganism(organism3, 4, 4);
+    for(int i = 0; i < 10; i++){
+      organism1.move();
+      organism2.move();
+      organism3.move();
+      System.out.println("Koniec tury numer: "+(i+1)+" Pozycja graczy: 1. x: "+ organism1.position.getX() + ", y: " + organism1.position.getY() + " ||| "+ " 2. x: " + organism2.position.getX() + ", y: " + organism2.position.getY() + " ||| "+" 3. x: "+organism3.position.getX()+", y: "+organism3.position.getY());
+    }
+    System.out.println("\nLiczba wygranych pojedynków graczy oraz końcowa energia:\n1. "+organism1.getCountBattles()+", "+organism1.getEnergy()+"\n2. "+organism2.getCountBattles()+", "+organism2.getEnergy()+"\n3. "+organism3.getCountBattles()+", "+organism3.getEnergy());
   }
 }
