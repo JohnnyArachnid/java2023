6fc7a75cf6cc43ef7b1b4853bc07e84e3b1b83fb
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2023-11-15 12:29:59 +0100

    Zmiany

--- src/main/java/org/life/Board.java
+++ src/main/java/org/life/Board.java
@@ -2,9 +2,9 @@ package org.life;
 
 public class Board {
 
-  private static int width;
-  private static int height;
-  private static Organism[][] organisms;
+  private int width;
+  private int height;
+  private Organism[][] organisms;
 
   public Board(int width, int height) {
     this.width = width;
@@ -21,31 +21,14 @@ public class Board {
     }
   }
 
-  public static void moveOrganism(Organism organism, int newX, int newY) {
+  public void moveOrganism(Organism organism, int newX, int newY) {
+    // TODO implement that one organism eats the other
     if (newX >= 0 && newX < width && newY >= 0 && newY < height && organisms[newX][newY] == null) {
       organisms[organism.getPosition().getX()][organism.getPosition().getY()] = null;
       organisms[newX][newY] = organism;
       organism.setPosition(new Position(newX, newY));
-    } else if (newX >= 0 && newX < width && newY >= 0 && newY < height && organisms[newX][newY] != null) {
-      int numer = organism.fight(organisms[newX][newY]);
-      if (numer == 1) {
-        organisms[organism.getPosition().getX()][organism.getPosition().getY()] = null;
-        organisms[newX][newY] = null;
-        organisms[newX][newY] = organism;
-        organism.setPosition(new Position(newX, newY));
-
-      } else if (numer == 2) {
-        organisms[organism.getPosition().getX()][organism.getPosition().getY()] = null;
-      }
-    }
-    else if (newX == -1) {
-      moveOrganism(organism, newX+2, newY);
-    } else if (newX == width) {
-      moveOrganism(organism, newX-2, newY);
-    } else if (newY == -1) {
-      moveOrganism(organism, newX, newY+2);
-    } else if (newY == height) {
-      moveOrganism(organism, newX, newY-2);
+    } else {
+      System.out.println("Invalid move!");
     }
   }
 }
