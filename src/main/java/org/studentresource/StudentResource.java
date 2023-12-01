ee65042a06a8ae722cfc0160bcd5294defee2a79
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2023-12-01 12:40:00 +0100

    Zmiany

--- src/main/java/org/studentresource/StudentResource.java
+++
@@ -1,9 +0,0 @@
-package org.studentresource;
-
-public interface StudentResource {
-    String getId();
-    String getName();
-    void setId(String id);
-    void setName(String name);
-    // Add other necessary methods here
-}
